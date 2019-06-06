db62d2a039f59ecf42e96134d33064f9b69f5118
yuu <yuuxzy@gmail.com>
2019-06-06 14:57:18 +0900

    Initial Commit

---
+++ ReduxSample.swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReduxSample.swift
+//
+//  Created by Yusuke Binsaki on 2019/06/06.
+//  Copyright © 2019 Yusuke Binsaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
