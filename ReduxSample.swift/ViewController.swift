b6e3c72859c60023f9c61a09bcb8103323fa68f6
yuu <yuuxzy@gmail.com>
2019-06-09 16:53:16 +0900

    remove

--- ReduxSample.swift/ViewController.swift
+++ ReduxSample.swift/ViewController.swift
@@ -7,19 +7,8 @@
 //
 
 import UIKit
-import ReSwift
 
-struct CounterActionIncrease: Action {}
-struct CounterActionDecrease: Action {}
-
-enum OtherAction: Action {
-    case Ok(Int)
-    case Err(String)
-}
-
-class ViewController: UIViewController, StoreSubscriber {
-
-    @IBOutlet var counterLabel: UILabel!
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,29 +16,8 @@ class ViewController: UIViewController, StoreSubscriber {
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        mainStore.subscribe(self) { (state) -> Subscription<CounterState> in
-            state.select { $0.counterState }
-        }
     }
 
     override func viewWillDisappear(_ animated: Bool) {
-        mainStore.unsubscribe(self)
-    }
-
-    func newState(state: CounterState) {
-        print("received state=\(state.counter)")
-        self.counterLabel.text = "\(state.counter)"
-    }
-
-    @IBAction func increaseButtonTapped(_ sender: UIButton) {
-        mainStore.dispatch(CounterActionIncrease())
-    }
-
-    @IBAction func decreaseButtonTapped(_ sender: UIButton) {
-        mainStore.dispatch(CounterActionDecrease())
-    }
-
-    @IBAction func otherActionTapped(_ sender: UIButton) {
-        mainStore.dispatch(OtherAction.Ok(0))
     }
 }
